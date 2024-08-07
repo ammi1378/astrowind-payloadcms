181d56783f0a51c4ad9021fd03efa9ab7bdac5c0
ammi1378 <ammi1378@gmail.com>
2024-08-07 11:59:20 +0330

    Fix beta install issues & add sharp

--- src/modules/core/payload/payload.config.ts
+++ src/modules/core/payload/payload.config.ts
@@ -5,12 +5,23 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { Users } from '@/collections/Users'
+import { Media } from '@/collections/Media'
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
+
+
+// const generateTitle: GenerateTitle<Page> = ({ doc }) => {
+//   return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
+// }
+
+// const generateURL: GenerateURL<Page> = ({ doc }) => {
+//   return doc?.slug
+//     ? `${process.env.NEXT_PUBLIC_SERVER_URL}/${doc.slug}`
+//     : process.env.NEXT_PUBLIC_SERVER_URL
+// }
 
 export default buildConfig({
   admin: {
@@ -25,8 +36,15 @@ export default buildConfig({
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || '',
   }),
+  // cors: [process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(Boolean),
+  // csrf: [process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(Boolean),
   sharp,
   plugins: [
+    // seoPlugin({
+    //   generateTitle,
+    //   generateURL,
+    // }),
     // storage-adapter-placeholder
   ],
+
 })
